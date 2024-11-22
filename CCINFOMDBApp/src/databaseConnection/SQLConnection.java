a5f4c742796ea0df2917c72ce21a1232c5431693
meia0528 <111009428+meia0528@users.noreply.github.com>
2024-11-22 14:54:25 +0800

    Update SQLConnection.java

--- CCINFOMDBApp/src/databaseConnection/SQLConnection.java
+++ CCINFOMDBApp/src/databaseConnection/SQLConnection.java
@@ -2,24 +2,26 @@ package databaseConnection;
 
 import java.sql.*;
 
+// DO NOT EDIT THIS! only edit the PASSWORD to whatever your password is for workbench
+// make sure you have installed the https://dev.mysql.com/downloads/connector/j/ and build path -> add external jar
+// you may look at other sources like stackoverflow and yt if there's an error -> make sure that you have "Connection successful!" when 
+// you run the file
+// you may remove the comment indicator in main to see if this file works first on your end
+
 public class SQLConnection {
-	
-	// make sure you have installed the https://dev.mysql.com/downloads/connector/j/ and build path -> add external jar
-	// you may look at other sources like stackoverflow and yt if there's an error -> make sure that you have "Connection successful!" when 
-	// you run the file
 
     public static Connection getConnection() {
         Connection conn = null;
 
         try {
-    
+            // Database connection details
             String URL = "jdbc:mysql://localhost:3306/dbconveniencestore";
-            String USERNAME = "root"; 
-            String PASSWORD = "**"; // Modify to your own password
+            String USERNAME = "root";
+            String PASSWORD = "***"; // Modify to your own password
 
             // Establish the connection
             conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            
+
             if (conn != null) {
                 System.out.println("Connection successful!");
             }
@@ -29,19 +31,11 @@ public class SQLConnection {
             System.out.println(ex.getMessage());
         }
 
-        return conn; 
+        return conn;
     }
 
-    public static void main(String[] args) {
-        Connection conn = SQLConnection.getConnection();
-        
-        if (conn != null) {
-            try {
-                // Close the connection after test
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    //public static void main(String[] args) {
+        // Test the connection
+    //    getConnection();
+    //}
 }
